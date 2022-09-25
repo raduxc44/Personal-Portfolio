459257013eead150987c71fe919431d096d58873
raduxc44 <raduxc44@gmail.com>
2022-09-25 05:13:25 +0300

    Added a head section with the corresponding styling

--- personal-portofolio/src/App.js
+++ personal-portofolio/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Nav from './components/Nav';
+import Head from './components/Nav/Head/Head';
+import Nav from './components/Nav/Nav/Nav';
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Head />
     </div>
   );
 }
---
+++ personal-portofolio/src/components/Nav/Head/Head.jsx
@@ -0,0 +1,12 @@
+import './Head.css'
+
+function Head () {
+    return (
+        <header>
+            <h1>Radu Ciocan</h1>
+            <h2>Web developer</h2>
+        </header>
+    )
+}
+
+export default Head
