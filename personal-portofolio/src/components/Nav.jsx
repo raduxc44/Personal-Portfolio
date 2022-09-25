568e20af1d506b7d51918ba8f3d9802d6a5f7cdd
raduxc44 <raduxc44@gmail.com>
2022-09-25 03:49:11 +0300

    Navbar dummy and cover added

---
+++ personal-portofolio/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import './Nav.css'
+
+function Nav () 
+{return(
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Projects</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)}
+
+export default Nav
