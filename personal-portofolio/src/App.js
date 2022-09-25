8a67846c5662c7c95e4ecfd0c21a28e4f2d72f1e
raduxc44 <raduxc44@gmail.com>
2022-09-26 00:41:20 +0300

    Implemented an about section with placeholder content

--- personal-portofolio/src/App.js
+++ personal-portofolio/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Head from './components/Nav/Head/Head';
 import Nav from './components/Nav/Nav/Nav';
+import About from './components/Nav/About/About';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Head />
+      <About />
     </div>
   );
 }
---
+++ personal-portofolio/src/components/Nav/About/About.jsx
@@ -0,0 +1,37 @@
+import './About.css'
+import Profiledummy from '../../../images/abstract-user-flat-1.svg'
+import TypeWriter from 'typewriter-effect'
+
+function About () {
+    return (
+        <div className="about">
+            <div className="pic-typewriter-container">
+                <div className="pic-container">
+                    <img src={Profiledummy} alt="ProfilePic" />
+                </div>
+                <div className='typewriter-container'>
+                    <p id='enjoy'>I ENJOY</p>
+                    <>
+                        <TypeWriter
+                            options={{
+                                strings: ['Pizza', 'Swimming', 'Reading', 'Videogames'],
+                                autoStart: true,
+                                loop: true,
+                                deleteSpeed: 100
+                            }}
+                        />
+                    </>
+                </div>
+            </div>
+            <div className='about-text-container'>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
+                Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
+                Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
+                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
