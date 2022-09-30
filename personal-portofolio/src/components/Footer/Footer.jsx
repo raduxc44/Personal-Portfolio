11c29e1b35b9cc9eeb0fec37e0f7cf003a05413e
raduxc44 <raduxc44@gmail.com>
2022-09-30 03:45:20 +0300

    Added a temporary footer

---
+++ personal-portofolio/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+function Footer () {
+    return(
+        <div className='footer-container'>
+            <h3>Contact</h3>
+            <footer>
+                <i class="fa-brands icon fa-github"></i>
+                <i class="fa-brands icon fa-linkedin-in"></i>
+                <i class="fa-solid icon fa-envelope"></i>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
