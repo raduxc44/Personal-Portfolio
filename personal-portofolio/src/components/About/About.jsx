5c0f23a7a48bdfd8539770716cf66e7f431062fb
raduxc44 <raduxc44@gmail.com>
2022-10-01 07:08:29 +0300

    Added a main header

--- personal-portofolio/src/components/About/About.jsx
+++ personal-portofolio/src/components/About/About.jsx
@@ -4,41 +4,44 @@ import TypeWriter from 'typewriter-effect'
 
 function About () {
     return (
-        <div className="about">
-            <div className="pic-typewriter-container">
-                <div className="pic-container">
-                    <img src={Profiledummy} alt="ProfilePic" />
+        <div className="about" id='about'>
+            <p className='about-main'>About me</p>
+            <div className='about-content'>
+                <div className="pic-typewriter-container">
+                    <div className="pic-container">
+                        <img src={Profiledummy} alt="ProfilePic" />
+                    </div>
+                    <div className='typewriter-container'>
+                        <p id='enjoy'>I ENJOY</p>
+                        <>
+                            <TypeWriter
+                                options={{
+                                    strings: ['Pizza', 'Swimming', 'Reading', 'Videogames'],
+                                    autoStart: true,
+                                    loop: true,
+                                    deleteSpeed: 100
+                                }}
+                            />
+                        </>
+                    </div>
                 </div>
-                <div className='typewriter-container'>
-                    <p id='enjoy'>I ENJOY</p>
-                    <>
-                        <TypeWriter
-                            options={{
-                                strings: ['Pizza', 'Swimming', 'Reading', 'Videogames'],
-                                autoStart: true,
-                                loop: true,
-                                deleteSpeed: 100
-                            }}
-                        />
-                    </>
+                <div className='about-text-container'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
+                    Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
+                    Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
+                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
+                    Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
+                    Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
+                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    </p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
+                    Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
+                    Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
+                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                    </p>
                 </div>
-            </div>
-            <div className='about-text-container'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
-                Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
-                Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
-                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
-                Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
-                Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
-                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                </p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br></br> 
-                Phasellus ex neque, suscipit a felis ut, lacinia accumsan odio.<br></br> 
-                Cras non urna id sem viverra gravida. Aliquam vel est vel libero euismod imperdiet.<br></br> 
-                Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-                </p>
             </div>
         </div>
     )
