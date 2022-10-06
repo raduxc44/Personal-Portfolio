74cffb4d3cf148eca7a57db46432cb42123b565c
raduxc44 <raduxc44@gmail.com>
2022-10-06 17:15:21 +0300

    Update Nav.jsx

--- personal-portofolio/src/components/Nav/Nav.jsx
+++ personal-portofolio/src/components/Nav/Nav.jsx
@@ -2,24 +2,20 @@ import './Nav.css'
 
 function Nav () {
 
-    let debounce = false;
     function navAnimateWhenPressed (e) {
-    if (debounce === true) return;
-    debounce = true;
     e.target.classList.add('gelatine')
     setTimeout(function(){
             e.target.classList.remove('gelatine')
-            debounce = false;
         }, 1000);
     }
 
     return(
     <nav>
         <ul>
-            <a onClick={navAnimateWhenPressed} href="#home"><li>Home</li></a>
-            <a onClick={navAnimateWhenPressed} href="#about"><li>About</li></a>
-            <a onClick={navAnimateWhenPressed} href="#projects"><li>Projects</li></a>
-            <a onClick={navAnimateWhenPressed} href="#footer"><li>Contact</li></a>
+            <a onClick={navAnimateWhenPressed} href="#home"><li className='noselect'>Home</li></a>
+            <a onClick={navAnimateWhenPressed} href="#about"><li className='noselect'>About</li></a>
+            <a onClick={navAnimateWhenPressed} href="#projects"><li className='noselect'>Projects</li></a>
+            <a onClick={navAnimateWhenPressed} href="#footer"><li className='noselect'>Contact</li></a>
         </ul>
     </nav>
 )}
