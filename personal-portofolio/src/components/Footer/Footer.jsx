eb4668979c423477ae856ff1720c77d7930e9ebe
raduxc44 <raduxc44@gmail.com>
2022-10-06 15:35:55 +0300

    Refined the icons styling

--- personal-portofolio/src/components/Footer/Footer.jsx
+++ personal-portofolio/src/components/Footer/Footer.jsx
@@ -4,31 +4,31 @@ import "animate.css/animate.min.css";
 
 function Footer () {
     return(
-        <div className='footer-container' id='footer'>
+        <div className='noselect footer-container' id='footer'>
             <h3>Contact</h3>
             <footer>
-                <AnimationOnScroll offset='10' animateIn="animate__bounceIn">
+                <AnimationOnScroll offset='10' animateIn="animate__bounceIn" animateOnce='true'>
                     <div className='contact-individual'>
                         <a href='https://github.com/raduxc44' target="new"><i className="fa-brands icon fa-github"></i></a>
                         <p>GitHub</p>
                     </div>
                 </AnimationOnScroll>
-                <AnimationOnScroll offset='10' animateIn="animate__bounceIn">
+                <AnimationOnScroll offset='10' animateIn="animate__bounceIn" animateOnce='true'>
                     <div className='contact-individual'>
                         <i className="fa-brands icon fa-linkedin-in"></i>
                         <p>LinkedIn</p>
                     </div>
                 </AnimationOnScroll>
-                <AnimationOnScroll offset='10' animateIn="animate__bounceIn">
+                <AnimationOnScroll offset='10' animateIn="animate__bounceIn" animateOnce='true'>
                     <div className='contact-individual'>    
                         <a href='mailto:raduxc44@gmail.com'><i className="fa-solid icon fa-envelope"></i></a>
                         <p>Mail</p>
                     </div>
                 </AnimationOnScroll>
-                <AnimationOnScroll offset='10' animateIn="animate__bounceIn">
+                <AnimationOnScroll offset='10' animateIn="animate__bounceIn" animateOnce='true'>
                     <div className='contact-individual'>
-                        <i className="fa-solid icon fa-file"></i>
-                        <p>Resume</p>
+                        <a href='../src/resume.pdf' target= "new"><i className="fa-solid icon fa-file"></i></a>
+                        <p id='contact-text'>Resume</p>
                     </div>
                 </AnimationOnScroll>
             </footer>
